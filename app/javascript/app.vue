6b5b65a3a9e8754cdb9283e817b89d3b4a13148b
morikann <mrknt0088@gmail.com>
2020-11-06 20:17:53 +0900

    issue'#3'の実装
    Vuetifyの導入

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,26 @@
 <template>
   <div id="app">
+    <v-app>
+      <v-btn class="mx-2" fab dark large color="purple">
+        <v-icon dark>
+          mdi-android
+        </v-icon>
+      </v-btn>
+
+      <v-icon large color="green darken-2">
+        mdi-domain
+      </v-icon>
+    </v-app>
+    <v-app>
+      <v-btn class="mx-2" fab dark small color="pink">
+        <v-icon>
+          mdi-heart
+        </v-icon>
+      </v-btn>
+    </v-app>
+
+   
+
     <p>{{ message }}</p>
   </div>
 </template>
